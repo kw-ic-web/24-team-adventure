e0b67d4ade0fbe89623a905ac24827d21df5384a
G <mungeon7142@gmail.com>
2024-11-07 00:19:03 +0900

    게임화면 마지막 레이아웃

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import GamePlay from '../pages/Game/GamePlay';
-
+import GameEnd from '../pages/Game/GameEnd';
 
 export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/gameplay" element={<GamePlay />} />
+        <Route path="/gameend" element={<GameEnd />} />
       </Routes>
     </BrowserRouter>
   );
