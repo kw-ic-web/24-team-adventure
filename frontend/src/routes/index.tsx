25b965c717031b6137f16705d197f486a5147528
G <mungeon7142@gmail.com>
2024-11-07 00:13:34 +0900

    게임화면 레이아웃 변경

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,16 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import GamePlay from '../pages/Game/GamePlay';
-//import GamePlayf from '../pages/Game/GamePlayFirst';
-//import GameEnd from '../pages/Game/GameEnd';
+
 
 export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/game_play" element={<GamePlay />} />
-        {/* <Route path="/game_playf" element={<GamePlayf />} />
-        <Route path="/game_end" element={<GameEnd />} /> */}
+        <Route path="/gameplay" element={<GamePlay />} />
       </Routes>
     </BrowserRouter>
   );
