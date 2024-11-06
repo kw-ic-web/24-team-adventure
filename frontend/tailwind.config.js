41f2feceb6f8ba772406a0684075c4868163d162
윤아 남 <yoona21978@gmail.com>
2024-11-07 01:05:17 +0900

    chore: prettier +a 수정

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,12 +1,13 @@
 // tailwind.config.js
 module.exports = {
-  content: ['./src/**/*.{ts,tsx}'],  // Tailwind CSS가 적용될 파일들을 지정
+  content: ['./src/**/*.{ts,tsx,css}', './src/index.css'], // Tailwind CSS가 적용될 파일들을 지정
   theme: {
     extend: {
       fontFamily: {
-        'noto': ['"Noto Sans KR"', 'sans-serif'],  // Noto Sans KR 폰트 설정 추가
+        noto: ['"Noto Sans KR"', 'sans-serif'], // Noto Sans KR 폰트 설정 추가
       },
     },
   },
   plugins: [],
 };
+//배경색1, 배경색2 설정
