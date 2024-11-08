eb751819eadc438b99ba6f706a822cbb6f3ddad9
Yeongil-Park <p03479@naver.com>
2024-11-08 14:46:16 +0900

    라우터 수정

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -14,7 +14,7 @@ import PostDetail from '../pages/Board/PostDetail';
 import PrivateRoute from './PrivateRoute';
 import PublicRoute from './PublicRoute';
 
-export default function AppRouter() {
+export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
@@ -27,16 +27,20 @@ export default function AppRouter() {
 
         {/* 화상 채팅 경로 */}
         <Route path="/video-chat" element={<VideoChat />} />
+        {/* video/end 변경 예정 */}
         <Route path="/video-end" element={<VideoChatEndPage />} />
 
         {/* 게임 관련 경로 */}
         <Route path="/games" element={<GameStart />} />
+        {/* 게임 API가 만들어지면, 각 동화(게임)별로 URL 이동 ex. /games/:gameId */}
         <Route path="/gameplay" element={<GamePlay />} />
+        {/* 마찬가지로 /games/:gameId/result 변경 예정 */}
         <Route path="/gameend" element={<GameEnd />} />
 
         {/* 게시판 경로 */}
         <Route path="/board" element={<StoryGrid />} />
         <Route path="/board/:story_id" element={<BoardPage />} />
+        {/* post는 수정 예정 */}
         <Route path="/board/:story_id/post/:geul_id" element={<PostDetail />} />
       </Routes>
     </BrowserRouter>
