e690b055b419065f57fb77fa455f7e79d414c42d
윤아 남 <yoona21978@gmail.com>
2024-11-27 21:51:19 +0900

    fix: 스타트화면 코드 정리

--- frontend/src/pages/Start/index.tsx
+++ frontend/src/pages/Start/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { GoogleLogin } from '@react-oauth/google';
-import UserList from '../../components/ui/Userlist';
 import useGoogleAuthMutation from '../../hooks/auth/useGoogleAuthMutation';
 import StartBackground from '../../components/ui/StartBackground';
 
@@ -8,17 +7,6 @@ import StartBackground from '../../components/ui/StartBackground';
 To do:
 다른 사용자 목록 박스 : 사용자 db 연결, 본인 외 나머지
 */
-
-interface User {
-  id: number;
-  name: string;
-  online: boolean;
-}
-const users: User[] = [
-  { id: 1, name: 'user1', online: true },
-  { id: 2, name: 'user2', online: false },
-  // 추가 사용자 데이터...
-];
 
 export default function Start() {
   // TanStack Query의 useMutation을 사용하여 구글 로그인 후 인증 요청 처리
@@ -39,11 +27,6 @@ export default function Start() {
     <div>
       {' '}
       <StartBackground />
-      {/* 실시간 사용자 목록 박스 */}
-      <div>
-        <UserList users={users} />
-      </div>
-      {/* 화면 중앙에 로고와 구글 로그인 버튼 추가 */}
       <div className="flex flex-col items-center justify-around h-screen">
         {/* 프로그램 로고 */}
         <img
