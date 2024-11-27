7031cbb6cda98365f9180d6a47a0a3b2112e3b6c
윤아 남 <yoona21978@gmail.com>
2024-11-27 21:48:14 +0900

    fix: 홈화면 스토리불러오기 제거,  스타트로고 위치 조정

--- frontend/src/pages/Start/index.tsx
+++ frontend/src/pages/Start/index.tsx
@@ -4,7 +4,6 @@ import UserList from '../../components/ui/Userlist';
 import useGoogleAuthMutation from '../../hooks/auth/useGoogleAuthMutation';
 import StartBackground from '../../components/ui/StartBackground';
 
-
 /*
 To do:
 다른 사용자 목록 박스 : 사용자 db 연결, 본인 외 나머지
@@ -37,25 +36,28 @@ export default function Start() {
   };
 
   return (
-    <div> <StartBackground />
+    <div>
+      {' '}
+      <StartBackground />
       {/* 실시간 사용자 목록 박스 */}
       <div>
         <UserList users={users} />
       </div>
       {/* 화면 중앙에 로고와 구글 로그인 버튼 추가 */}
-      <div className="relative flex flex-col items-center justify-around pt-45 h-screen ">
+      <div className="flex flex-col items-center justify-around h-screen">
         {/* 프로그램 로고 */}
-
-        <img src="/images/startlogo.png" alt="Game Logo"  className="w-[900px] h-auto" />
-
-        <div className="mt-[-150px]" >
+        <img
+          src="/images/startlogo.png"
+          alt="Game Logo"
+          className="w-[900px] h-auto"
+        />
+        <div className="mt-[-150px]">
           {/* 구글 로그인 버튼 */}
           <GoogleLogin
             onSuccess={handleLoginSuccess} // 로그인 성공 시 호출되는 함수
             onError={handleLoginFailure} // 로그인 실패 시 호출되는 함수
             width={'220px'}
             useOneTap // One Tap 로그인 기능을 활성화
-            
           />
         </div>
       </div>
