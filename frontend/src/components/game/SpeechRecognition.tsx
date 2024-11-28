f1a76bb165eef1de3056a3553716a8e54bc8e26b
G <mungeon7142@gmail.com>
2024-11-29 00:09:30 +0900

    fix : mic버튼 수정

--- frontend/src/components/game/SpeechRecognition.tsx
+++ frontend/src/components/game/SpeechRecognition.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-
+import micIcon from './mic.svg';
 // 전역 타입 선언
 declare global {
   interface Window {
@@ -47,11 +47,23 @@ export default function SpeechRecognition({
   return (
     <button
       onClick={toggleRecognition}
-      className={`p-4 rounded-full shadow-lg font-bold ${
-        recognizing ? 'bg-red-600 text-white' : 'bg-green-500 text-white'
+      className={`p-4 rounded-full shadow-lg font-bold flex items-center justify-center ${
+        recognizing
+          ? 'bg-red-600 animate-pulse'
+          : 'bg-gray-300 hover:bg-gray-400'
       }`}
     >
-      {recognizing ? '인식 정지' : '인식 시작'}
+      <img
+        src={micIcon}
+        alt="Microphone Icon"
+        className="w-6 h-6"
+        style={{
+          width: '40px', // 너비 48px
+          height: '35px', // 높이 48px
+          transition: 'transform 0.3s ease',
+          transform: recognizing ? 'scale(1.2)' : 'scale(1)',
+        }}
+      />
     </button>
   );
 }
