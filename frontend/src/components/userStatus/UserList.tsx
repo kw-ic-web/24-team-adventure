73010b909ec9cbb4944a9828dfeb590c7b9d7324
G <mungeon7142@gmail.com>
2024-12-02 00:32:17 +0900

    feat : 기존 레이아웃에 실시간유저기능 적용

--- frontend/src/components/userStatus/UserList.tsx
+++ frontend/src/components/userStatus/UserList.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../ui/CommonUi.css';
 
 // 사용자 데이터 타입 정의
 interface User {
@@ -12,6 +13,7 @@ interface UserListProps {
   users: User[]; // `users` prop의 타입
 }
 
+// 컴포넌트 정의
 function UserList({ users }: UserListProps) {
   // 데이터가 없을 경우 처리
   if (!users || users.length === 0) {
@@ -19,15 +21,28 @@ function UserList({ users }: UserListProps) {
   }
 
   return (
-    <div>
-      <h2>All Users</h2>
-      <ul>
-        {users.map((user) => (
-          <li key={user.id}>
-            <strong>{user.name}</strong> - {user.online ? 'Online' : 'Offline'}
-          </li>
-        ))}
-      </ul>
+    <div className="user-list-box">
+      {users.map((user) => (
+        <div key={user.id} className="user-list-item">
+          <div className="user-item-flex">
+            <span role="img" aria-label="user-profile" className="text-xl">
+              👤
+            </span>
+            <span>{user.name}</span>
+          </div>
+          <div className="status-dot">
+            <img
+              src={
+                user.online
+                  ? '/images/online_icon.png'
+                  : '/images/offline_icon.png'
+              }
+              alt="status"
+              className="status-icon"
+            />
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
