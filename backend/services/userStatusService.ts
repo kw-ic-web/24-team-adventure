4d21147ce6529f6dabd3dc186b7b1a85dde63717
G <mungeon7142@gmail.com>
2024-12-06 17:41:43 +0900

    fix : 상태확인 시간변경

--- backend/services/userStatusService.ts
+++ backend/services/userStatusService.ts
@@ -2,14 +2,13 @@ import supabase from "../config/supabaseClient";
 
 export const markInactiveUsersOffline = async (): Promise<void> => {
   try {
-    // 현재 시간 기준으로 5분 전 시간 계산 (ISO 8601 형식 변환)
-    const fiveMinutesAgo = new Date(Date.now() - 5 * 60 * 1000).toISOString();
+    const MinutesAgo = new Date(Date.now() - 20 * 60 * 1000).toISOString();
 
     const { data, error } = await supabase
       .from("user")
-      .update({ online: false }) // 온라인 상태를 오프라인으로 변경
-      .lt("updated_at", fiveMinutesAgo) // 5분 전 시간 기준
-      .eq("online", true); // 현재 온라인 상태인 사용자만 선택
+      .update({ online: false })
+      .lt("updated_at", MinutesAgo)
+      .eq("online", true);
 
     if (error) throw error;
 
